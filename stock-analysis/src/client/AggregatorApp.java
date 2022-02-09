26b6ff9a0c9c91cf461b9c91c35932bf05705368
Victor Jason Presher <presher@users.noreply.github.com>
2022-02-09 16:25:12 -0600

    Update AggregatorApp.java

--- stock-analysis/src/client/AggregatorApp.java
+++ stock-analysis/src/client/AggregatorApp.java
@@ -10,7 +10,9 @@ import aggregators.MinAggregator;
 public class AggregatorApp {
 
 	public static void main(String[] args) throws IOException {
-		
+		//update the constructor for desired aggegator
+		//current aggregator is Mean, also use Min, and Max
+		//update the generics as well to match the constructor
 		MeanAggregator agg = new MeanAggregator();
 		AggregatorProcessor<MeanAggregator> aggsProcessor = new AggregatorProcessor<MeanAggregator>(agg, "table.csv");
 		double value = aggsProcessor.runAggregator(1);
